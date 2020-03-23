ab4fffdd5c49d6d14fc0f4535d311fcd773c46b3
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-23 16:04:22 +0100

    First Commit

---
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sounddemo;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.thunder);
+        mediaPlayer.start();
+    }
+}
