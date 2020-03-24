5fb1111929cbcec3a279eb9c1ee887dcca0bd179
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-24 12:45:53 +0100

    Play and Pause Button added

--- app/src/main/java/com/example/sounddemo/MainActivity.java
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -2,17 +2,34 @@ package com.example.sounddemo;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    MediaPlayer mediaPlayer;
+
+    public void play(View view) {
+
+        mediaPlayer.start();
+
+    }
+
+    public void pause(View view) {
+
+        mediaPlayer.pause();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.thunder);
-        mediaPlayer.start();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.thunder);
+
     }
 }
