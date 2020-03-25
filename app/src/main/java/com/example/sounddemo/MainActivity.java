b085d2da67cbea83c53739be2235d1f404a2dcbc
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-25 13:51:40 +0100

    Lautstärke mit SeekBar regelbar

--- app/src/main/java/com/example/sounddemo/MainActivity.java
+++ app/src/main/java/com/example/sounddemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sounddemo;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     MediaPlayer mediaPlayer;
+    SeekBar volumeControl;
+    AudioManager audioManager;
 
     public void play(View view) {
 
@@ -28,8 +33,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.thunder);
 
+        volumeControl = findViewById(R.id.volumeSeekBar);
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("seekBar Changed", Integer.toString(i));
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
